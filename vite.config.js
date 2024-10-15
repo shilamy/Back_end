f46e54ff09966a7642e846b75e5b2186b59cbb5d
shilamy <macmynev@gmail.com>
2024-10-15 18:03:39 +0300

    Updating the import path on the vite.config file.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
     base:'./',
